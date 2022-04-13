db296256734c64ca1573dcd662bb0bcc5aaa7329
Brian Parks <bnparks@ksu.edu>
2022-04-13 14:49:46 -0500

    Also these as well

---
+++ src/common/vector_utils.c
@@ -0,0 +1,42 @@
+#include "VectorUtils.h"
+#include <math.h>
+
+Vec3f newVec3f(float x, float y, float z) {
+    Vec3f ret;
+    ret.x= x;
+    ret.y = y;
+    ret.z = z;
+
+    return ret;
+}
+
+Vec3f vectorAdd(Vec3f u, Vec3f v) {
+    return newVec3f(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+
+Vec3f vectorScalarMult(float t, Vec3f u) {
+    return newVec3f(t*u.x, t*u.y, t*u.z);
+}
+
+Vec3f vectorNormalize(Vec3f u) {
+    float normalizationFactor = 1/sqrt(vectorDot(u, u));
+    return vectorScalarMult(normalizationFactor, u);
+}
+
+Vec3f ptToVector(Vec3f p1, Vec3f p2) {
+    return newVec3f(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+}
+
+Vec3f finalVel(Vec3f accel, Vec3f v0, double t) {
+    return newVec3f(accel.x * t + v0.x, accel.y * t + v0.y, accel.y * t + v0.y);
+}
+
+Vec3f finalPos(Vec3f accel, Vec3f v0, Vec3f p0, double t) {
+    return newVec3f(1/2 * accel.x * t * t + v0.x * t + p0.x, 
+                    1/2 * accel.y * t * t + v0.y * t + p0.y,
+                    1/2 * accel.z * t * t + v0.z * t + p0.z);
+}
+
+float vectorDot(Vec3f u, Vec3f v) {
+    return u.x * v.x + u.y * v.y + u.z * v.z;
+}
---
+++ src/common/vector_utils.h
@@ -0,0 +1,67 @@
+#pragma once
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vec3f;
+
+typedef struct {
+    Vec3f pos;
+    Vec3f vel;
+    float mass;
+} Body;
+
+/**
+ * @brief Creates a new vector with the given x,y,z parameters and stores it in out
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @param out 
+ */
+Vec3f newVec3f(float x, float y, float z);
+
+/**
+ * @brief Adds two vectors and stores the result in u
+ * 
+ * @param u 
+ * @param v 
+ */
+Vec3f vectorAdd(Vec3f u, Vec3f v);
+
+/**
+ * @brief Scales u by t
+ * 
+ * @param t 
+ * @param u 
+ */
+Vec3f vectorScalarMult(float t, Vec3f u);
+
+/**
+ * @brief Computes the vector dot product of u and v
+ * 
+ * @param u 
+ * @param v 
+ * @return float 
+ */
+float vectorDot(Vec3f u, Vec3f v);
+
+/**
+ * @brief Converts vector u to a unit vector
+ * 
+ * @param u 
+ */
+Vec3f vectorNormalize(Vec3f u);
+
+/**
+ * @brief Constructs a vector starting at p1 pointing towards p2
+ * 
+ * @param p1 
+ * @param p2 
+ * @return double* 
+ */
+Vec3f ptToVector(Vec3f p1, Vec3f p2);
+
+Vec3f finalVel(Vec3f accel, Vec3f v0, double t);
+
+Vec3f finalPos(Vec3f accel, Vec3f v0, Vec3f p0, double t);
