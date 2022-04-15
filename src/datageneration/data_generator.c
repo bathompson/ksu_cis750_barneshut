3f5ba8dd37c2715ef45a6cd0af047e43cd239389
Brian Parks <bnparks@ksu.edu>
2022-04-15 10:32:30 -0500

    Works, but gives memory error if nBodies < 4085

--- src/datageneration/data_generator.c
+++ src/datageneration/data_generator.c
@@ -5,7 +5,16 @@
 #include "../common/vector_utils.h"
 #include "../common/fileIO_util.h"
 
+/**
+ * @brief Create a random float.
+ * 
+ * @param basisPoint The center point to base the values around.
+ * @param scaleFactor The range of the values from around the basis point.
+ * @param enableInversion Enable inverting the polarity.
+ * @return float The random float.
+ */
 float createRandom(float basisPoint, float scaleFactor, int enableInversion);
+//Defined up here to save some passing.
 float globalSize;
 
 /**
@@ -26,28 +35,32 @@ int main(int argc, char **argv) {
     float percentBodiesInClusters;
     float scaleFactor;
     float maximumMass;
+    float velocityMultipler;
     size_t numBodies;
     size_t numClusters;
     size_t numFreeBodies;
     size_t numBodiesInClusters;
-    size_t yeet;
+    size_t constantIndex;
     Body *bodies;
     
     //Check Arguments
-    if(argc != 7) {
-        printf("Invalid input format. Format via: globalSize[0,sizeof(float)], ");
+    //Command args I have been using:
+    //100000000000 10000 15 .8 .05 500000 .00005
+    if(argc != 8) {
+        printf("Invalid input format. Format via: globalSize[0,MAX_FLOAT], ");
         printf("numBodies[n], numClusters[n], percentBodiesInClusters[0,1], clusterDistRatio[0,1], ");
-        printf("maximumMass[0, sizeof(float)]\n");
+        printf("maximumMass[0,MAX_FLOAT], velocityMultiplier[0,1]\n");
         return -1;
     }
 
     //Convert input data.
     globalSize = atof(argv[1]);
-    numBodies = strtoul(argv[2], NULL, 10);
+    numBodies = strtoul(argv[2], NULL, 10); //If you use less than 4085 you get a corrupted top size.
     numClusters = strtoul(argv[3], NULL, 10);
     percentBodiesInClusters = atof(argv[4]);
     clusterDistRatio = atof(argv[5]);
     maximumMass = atof(argv[6]);
+    velocityMultipler = atof(argv[7]);
     scaleFactor = globalSize * clusterDistRatio;
 
     //Allocate memory, do some setup math, seed random.
@@ -57,22 +70,21 @@ int main(int argc, char **argv) {
     srand(time(NULL));
 
     //Calculate free bodies
-    for(yeet = 0; yeet < numFreeBodies; yeet++) {
+    for(constantIndex = 0; constantIndex < numFreeBodies; constantIndex++) {
         Vec3f posVector;
         Vec3f velVector;
         Body b;
         posVector.x = createRandom(0, globalSize, 1);
         posVector.y = createRandom(0, globalSize, 1);
         posVector.z = createRandom(0, globalSize, 1);
-        //Max velocity in any direction is 10% of scale factor.
-        velVector.x = createRandom(0, globalSize * .1, 1);
-        velVector.y = createRandom(0, globalSize * .1, 1);
-        velVector.z = createRandom(0, globalSize * .1, 1);
+        velVector.x = createRandom(0, globalSize * velocityMultipler, 1);
+        velVector.y = createRandom(0, globalSize * velocityMultipler, 1);
+        velVector.z = createRandom(0, globalSize * velocityMultipler, 1);
         b.mass = ((float) rand() / (float) (RAND_MAX)) * maximumMass;
         b.pos = posVector;
         b.vel = velVector;
 
-        bodies[yeet] = b;
+        bodies[constantIndex] = b;
     }
     
     //Calculate clusters
@@ -80,10 +92,7 @@ int main(int argc, char **argv) {
         float xBasis = createRandom(0, globalSize, 1);
         float yBasis = createRandom(0, globalSize, 1);
         float zBasis = createRandom(0, globalSize, 1);
-        size_t bodiesInGivenCluster = (numBodiesInClusters / numClusters) + (numBodiesInClusters % numClusters < i ? 0 : 1);
-        for(int j = 0; j < bodiesInGivenCluster; j++) {
-            //TODO: Figure out why we get nothing exported without this print statement.
-            printf("Some reason this is needed?\n");
+        for(int j = 0; j < (numBodiesInClusters / numClusters) + (numBodiesInClusters % numClusters < i ? 0 : 1); j++) {
             Vec3f posVector;
             Vec3f velVector;
             Body b;
@@ -91,15 +100,15 @@ int main(int argc, char **argv) {
             posVector.y = createRandom(yBasis, scaleFactor, 0);
             posVector.z = createRandom(zBasis, scaleFactor, 0);
             //Max velocity in any direction is 10% of scale factor.
-            velVector.x = createRandom(0, scaleFactor * .1, 0);
-            velVector.y = createRandom(0, scaleFactor * .1, 0);
-            velVector.z = createRandom(0, scaleFactor * .1, 0);
+            velVector.x = createRandom(0, scaleFactor * velocityMultipler, 0);
+            velVector.y = createRandom(0, scaleFactor * velocityMultipler, 0);
+            velVector.z = createRandom(0, scaleFactor * velocityMultipler, 0);
             b.mass = ((float) rand() / (float) (RAND_MAX)) * maximumMass;
             b.pos = posVector;
             b.vel = velVector;
             
-            bodies[yeet] = b;
-            yeet++;
+            bodies[constantIndex] = b;
+            constantIndex++;
         }
     }
     
@@ -107,20 +116,10 @@ int main(int argc, char **argv) {
     writeBodies("outputFile.csv", bodies, numBodies);
 
     //Free memory
-    //TODO: Figure out why we are getting this memory error.
     free(bodies);
-
     return 0;
 }
 
-/**
- * @brief Create a random float.
- * 
- * @param basisPoint The center point to base the values around.
- * @param scaleFactor The range of the values from around the basis point.
- * @param enableInversion Enable inverting the polarity.
- * @return float The random float.
- */
 float createRandom(float basisPoint, float scaleFactor, int enableInversion) {
     float ret; 
     do {
