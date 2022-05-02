ba0e4436908c408bb8c75b45363894c919de8fad
Ben Thompson <bathompson@ksu.edu>
2022-05-02 15:49:09 -0500

    Fixed issue with barnes hut force computations

--- src/serial/serial_barnes_hut.c
+++ src/serial/serial_barnes_hut.c
@@ -9,6 +9,10 @@
 
 Vec3f computeBarnesHutForce(Octree *root, Body *body, float theta)
 {
+    if(root == NULL)
+    {
+        return newVec3f(0,0,0);
+    }
     double scalarForce = 0;
     Vec3f vectorDist = ptToVector(body->pos, root->massPosition);
     float distSq = vectorDot(vectorDist, vectorDist);
